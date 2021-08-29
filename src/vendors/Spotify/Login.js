294bc4cf6784cd9f9b474e0cd5a96576093faa57
Mateus Henrique <mateus.henrique223@gmail.com>
2021-08-29 18:01:56 -0300

    spotify authorize method

--- src/vendors/Spotify/Login.js
+++ src/vendors/Spotify/Login.js
@@ -10,14 +10,25 @@ exports.login = async () => {
         scope=${scopes.join("%20")}&
         redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&
         state=${state}
-    `.replace(/\n| /gm,'')
+    `.replace(/\n| /gm, '')
     return redirectUrl
 }
 
 exports.authorize = async (code) => {
     if (code) {
-        await Spotify.get("/api/token")
-        return { success: 1 }
+        const tokenResponse = await Spotify.post("/api/token", {
+            code: code,
+            redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+            grant_type: 'authorization_code'
+        })
+        const { access_token } = tokenResponse.data
+        const resp = await Spotify.get("/me", {
+            headers: {
+                'Authorization': 'Bearer ' + access_token
+            }
+        })
+
+        return resp.data
     }
     return { success: 0 }
 }
