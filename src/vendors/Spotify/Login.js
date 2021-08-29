9d38944f4ae27e4e9eb960ab33e95b5a4d2161f4
Mateus Henrique <mateus.henrique223@gmail.com>
2021-08-29 17:49:53 -0300

    added playlist read scode

--- src/vendors/Spotify/Login.js
+++ src/vendors/Spotify/Login.js
@@ -3,9 +3,11 @@ const database = require('../../database')
 
 exports.login = async () => {
     const state = '34fFs29kd09'    //spotify recommended state
+    const scopes = ['playlist-read-private']
     const redirectUrl = `https://accounts.spotify.com/authorize?
         response_type=code&
         client_id=${process.env.SPOTIFY_CLIENT_ID}&
+        scope=${scopes.join("%20")}&
         redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&
         state=${state}
     `.replace(/\n| /gm,'')
@@ -14,7 +16,7 @@ exports.login = async () => {
 
 exports.authorize = async (code) => {
     if (code) {
-        await database.insert(code)
+        await Spotify.get("/api/token")
         return { success: 1 }
     }
     return { success: 0 }
